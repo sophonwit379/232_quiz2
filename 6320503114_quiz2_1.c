b779d06f54bfd37db36e89d1339d3b19dd79c460
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-25 18:35:25 +0700

    2

--- 6320503114_quiz2_1.c
+++ 6320503114_quiz2_1.c
@@ -1,27 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j,count=0,num,multi=2,check=2;
+    int i,n,j,count=0,num,multi=2,check=2,show=1,com=-1;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         num=i+1;
-        while(num>0)
+        while(num>1)
         {
-            if(num%2==0)
+            if(num%multi==0)
             {
-                num/=2;
                 if(check==multi)
                     count++;
+                else
+                    count=0;
+                num/=multi;
+                check=multi;
+                if(count==3)
+                    break;
             }
             else
                 multi++;
-            check=multi;
+        }
+        if(count<2)
+        {
+            show++;
         }
         multi=2;
+        check=2;
         count=0;
     }
-
+    printf("\n%d",show);
 
 
 
