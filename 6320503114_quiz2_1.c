9121674a6aaf58c2e6eb7e90ec1d8b9eb43cfe51
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-25 17:43:50 +0700

    1

--- 6320503114_quiz2_1.c
+++ 6320503114_quiz2_1.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 int main()
 {
+    int i,n,j,count,num,multi=2;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        num=i+1;
+        while(num>0)
+        {
+            if(num%2==0)
+            {
+                tmp=num%0;
 
+            }
+            else
+                multi++;
+        }
+        multi=2;
+    }
 
 
 
