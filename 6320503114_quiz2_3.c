1d993be042a6452c9cbebb8f2f8a2fb7cf0e16bc
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-25 17:34:59 +0700

    complete

--- 6320503114_quiz2_3.c
+++ 6320503114_quiz2_3.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,tmp[7],count,j=0,num,compare=0;
+    int i,n,tmp[7],count=0,j=0,num,compare=0,num2;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
+
+        num2=i+1;
         num=i+1;
         while(num>0)
         {
@@ -14,10 +16,12 @@ int main()
             compare+=tmp[j];
             j++;
         }
-
-
-
+        if(compare==num2)
+            count++;
+        j=0;
+        compare=0;
     }
+    printf("%d",count);
 
 
 
