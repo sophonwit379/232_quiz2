0f18ae5784c5abe7d4012afb999dfce3c3627302
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-25 19:27:57 +0700

    com check

--- 6320503114_quiz2_1.c
+++ 6320503114_quiz2_1.c
@@ -1,36 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j=0,count=0,num,multi=2,check[1000]={1},show=1,com=0;
+    int i,n,j=0,count=0,num,check[1000]={1},x,res;
     scanf("%d",&n);
-    for(i=0;i<n; i++)
+    for(i=0; i<n; i++)
     {
         num=i+1;
         if(num*num*num<=n)
         {
             check[j]=num*num*num;
         }
-        printf("%d\n",check[j]);
         if(check[j]==0)
             break;
         j++;
     }
-    j=1;
-    for(i=0;i<n; i++)
+    for(i=0; i<n; i++)
     {
         num=i+1;
-        while(num>1)
+        for(x=1;x<j;x++)
         {
-            if(i%check[j]==0)
+            if(num%check[x]==0)
             {
                 count++;
                 break;
             }
-            else
-                j++;
         }
     }
-
+    res=n-count;
+    printf("%d\n",res);
 
 
 
